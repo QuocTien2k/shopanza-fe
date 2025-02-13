52df155e461fbf2083b632ba9f728a8fd70bfdd2
QuocTien2k <hadesdqt@gmail.com>
2025-02-13 22:09:02 +0700

    config rule eslint and code ShopContext

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -1,15 +1,32 @@
 import { RiMoneyDollarCircleLine } from "react-icons/ri"
+import { TbTruckDelivery } from "react-icons/tb"
+import { BiSupport } from "react-icons/bi"
 
 
 const Features = () => {
     return (
-        <section>
+        <section className="max-padd-container mt-16">
             {/* Container */}
-            <div>
-                <div>
-                    <RiMoneyDollarCircleLine />
+            <div className="max-padd-container flexBetween flex-wrap gap-8 rounded-2xl">
+                <div className="flexCenter gap-x-3">
+                    <RiMoneyDollarCircleLine className="text-3xl" />
                     <div>
-                        <h4></h4>
+                        <h4 className="medium-15">MONEY-BACK GUARANTEE</h4>
+                        <p>100% refund guaranteed if you{"'"}re not satisfied.</p>
+                    </div>
+                </div>
+                <div className="flexCenter gap-x-3">
+                    <TbTruckDelivery className="text-3xl" />
+                    <div>
+                        <h4 className="medium-15">FREE SHIPPING & RETURNS</h4>
+                        <p>Free shipping availiable on all orders above $99.</p>
+                    </div>
+                </div>
+                <div className="flexCenter gap-x-3">
+                    <BiSupport className="text-3xl" />
+                    <div>
+                        <h4 className="medium-15">24/7 ONLINE SUPPORT</h4>
+                        <p>Our team is here to assist you round the clock </p>
                     </div>
                 </div>
             </div>
--- frontend/src/components/NewArrivals.jsx
+++ frontend/src/components/NewArrivals.jsx
@@ -1,8 +1,58 @@
+import { useEffect, useState } from "react";
+import Title from "./Title"
+import { products } from "../assets/data"
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
+import Item from "./Item";
 
 const NewArrivals = () => {
+    const [PopularProducts, setPopularProducts] = useState([]);
+    useEffect(() => {
+        const data = products.slice(0, 7);
+        //console.log(data);
+        setPopularProducts(data);
+    }, [products])
     return (
-        <div>NewArrivals</div>
+        <section className="max-padd-container pt-16">
+            <Title title1={'New'} title2={'Arivals'} titleStyles={'pb-10'} paraStyles={'!block'} />
+
+            {/* Container */}
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 4000,
+                    disableOnInteraction: false,
+                }}
+                breakpoints={{
+                    300: {
+                        slidesPerView: 2
+                    },
+                    666: {
+                        slidesPerView: 3
+                    },
+                    900: {
+                        slidesPerView: 4
+                    },
+                    1250: {
+                        slidesPerView: 5
+                    },
+                }}
+                modules={[Autoplay]}
+                className="h-[399px] mt-5"
+            >
+                {PopularProducts.map((product) => (
+                    <SwiperSlide key={product._id}>
+                        <Item product={product} />
+                    </SwiperSlide>
+
+                ))}
+
+            </Swiper>
+        </section>
     )
 }
 
